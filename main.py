61bbd011d6e6d318a0a7f20b75332dfab29257a3
HuiJuuu <81000484+HuiJuuu@users.noreply.github.com>
2023-04-02 14:00:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import juu
+def solution(id_list, report, k):
+    a = [0]*len(id_list)
+    b = {} # 누구를 누가 신고했는지 # key:신고당한, value:신고한
+    c = {} # 누가 누굴 신고했는지
+    for i in id_list:
+        b[i]=[]
+        c[i]=[]
+    for j in set(report): # 형변환으로 중복신고 거름
+        j = j.split(' ')
+        b[j[1]].append(j[0])
+        c[j[0]].append(j[1])
+    print(b)
+    print(c)
+
+    # for key.value in a.items():
+    #         if s in b:
+    #             s >= k
+    #             a[id_list.index()] += 1
+    count = 0
+    for v,s in b.items():
+        print(s)
+        if len(s) >= k:
+            if 
+            for i in s:
+                continue
+
+
+        #count += 1
+
+    return a
+solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2)
+#solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"],3)
--- test.py
+++ test.py
@@ -1,19 +1,32 @@
-import numpy as np
+def solution(new_id):
+    # 1단계 소문자 변경
+    new_id = new_id.lower()
 
-number, day = map(int, input().split())
-commute_list = list(map(int, input().split()))
-work_max_list = list(map(int, input().split()))
-schedule = []
+    # 2단계 !,@,#,* 제거
+    for i in new_id:
+        if not (i.islower() or i.isdigit() or i in {"-","_","."}):
+            new_id = new_id.replace(i,"")
 
-for i, num in enumerate(work_max_list):
-    schedule.append([])
-    schedule[i].append(num)
-    arr = np.array(commute_list)
-    indexes = arr.argsort()[::-1][:num]
-    for k in indexes:
-        if commute_list[k] == 0:
-            continue
-        schedule[i].append(k + 1)
-        commute_list[k] += 1
+    # 3단계 .. 제거
+    while '..' in new_id:
+        new_id = new_id.replace("..",".")
 
-print(schedule)
+    # 4단계 앞, 뒤 . 제거
+    new_id = new_id.strip(".")
+
+    # 5단계 빈 문자열 확인
+    if new_id == '':
+        new_id = "a"
+
+    # 6단계 15자 초과 제거
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        if new_id[14] == ".":
+            new_id = new_id[:-1]
+
+    # 7단계 3 미만 추가
+    while len(new_id) < 3:
+        plus = new_id[-1]
+        new_id+=plus
+
+    return new_id
